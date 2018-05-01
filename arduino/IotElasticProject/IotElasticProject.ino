8997c2da70b3e2ad593137d9bfd9db14d0b0a518
lunix983 <lunix983@gmail.com>
2018-05-01 11:14:11 +0200

    Minor improvement

--- arduino/IotElasticProject/IotElasticProject.ino
+++ arduino/IotElasticProject/IotElasticProject.ino
@@ -8,6 +8,16 @@ RH_RF95 rf95;
 //For product: LG01. 
 #define BAUDRATE 115200
 
+uint16_t crcdata = 0;
+uint16_t recCRCData = 0;
+float frequency = 868.0;
+String dataString = "";
+String temp_send = "";
+String umid_send = "";
+void uploadData(); // Upload Data to tempIntPartingSpeak.
+int sf = 12;
+long bw = 250000;
+int cr = 8;
 
 void setup()
 {
